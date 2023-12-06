0f8e4c7f22d2223fedb7731b3df97e4b85288a16
ENDER ÇELİK <182804017@ogr.cbu.edu.tr>
2023-12-06 22:48:37 +0300

    add how it works section

---
+++ src/components/layout/HowItWorks/HowItWorks.jsx
@@ -0,0 +1,67 @@
+import './howitworks.css';
+import blockchain from '/assets/svg/blockchain2.svg';
+import authenticated from '/assets/svg/authenticated.svg';
+import login from '/assets/svg/login.svg';
+
+export default function HowItWorks() {
+  return (
+    <section className='how-it-works-container mt-10'>
+      <div className='how-it-works-wrapper'>
+        <h1 className='text-6xl font-bold text-center'>How It Works</h1>
+        <div className='how-it-works-content'>
+          <div className='how-it-work-card'>
+            <div className='card-img h-28 w-20'>
+              <img src={login} />
+            </div>
+            <h3 className='card-title text-3xl font-semibold'>Login</h3>
+            <div className='card-description'>
+              After logging into the platform, the employer initiates the
+              verification process by clicking on the "Authenticate" button.
+              After login the employer provides the employee's name and school
+              identification number to kickstart the authentication process.
+            </div>
+          </div>
+          <div className='how-it-work-card'>
+            <div className='card-img h-28 w-20'>
+              <img src={blockchain} />
+            </div>
+            <h3 className='card-title text-3xl font-semibold'>
+              Verification & Blockchain
+            </h3>
+            <div className='card-description'>
+              <span className='font-bold'>Activation of Smart Contracts </span>:
+              The platform activates smart contracts on the blockchain to verify
+              the accuracy of the provided information. <br />
+              <br />{' '}
+              <span className='font-bold'>
+                Cross-Referencing with Blockchain Data{' '}
+              </span>
+              : The system cross-references the entered details with the data
+              stored on the blockchain, ensuring data consistency.
+            </div>
+          </div>
+          <div className='how-it-work-card'>
+            <div className='card-img h-28 w-20'>
+              <img src={authenticated} />
+            </div>
+            <h3 className='card-title text-3xl font-semibold'>
+              Access & Confirmation
+            </h3>
+            <div className='card-description'>
+              <span className='font-bold'>
+                Presentation of Verified Documents
+              </span>
+              : Upon successful verification, the platform retrieves and
+              presents all authenticated documents from the employee's school.
+              <br />
+              <br />
+              <span className='font-bold'>Employer Confirmation </span>: The
+              employer reviews the presented documents, confirming the validity
+              of the employee's educational credentials.
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
